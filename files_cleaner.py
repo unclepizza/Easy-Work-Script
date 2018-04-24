f3cc5e790a1472797a081f7674c2138592dccaff
gaok <gaok@trip.com>
2018-04-24 17:40:07 +0800

    重命名

--- files_cleaner.py
+++ files_cleaner.py
@@ -10,16 +10,28 @@ DELETE_PATTERN = re.compile('daemon-\w*\.out')  # 待删除文件名的格式
 
 
 # 对目标文件进行处理
-def do_operation(target_file):
+def do_operation(target_file_path):
     # 写入文件
-    # record_deleted_files_name(target_file)
+    # record_deleted_files_name(target_file_path)
     # 移动文件
-    # move_files_to_new_dir(target_file)
+    # move_file_to_new_dir(target_file_path)
     # 删除文件
-    saved_delete_files(target_file)
+    # saved_delete_file(target_file_path)
+    # 重命名
+    rename_file(target_file_path)
 
 
-def saved_delete_files(target_file):
+def rename_file(target_file_path):
+    if os.path.isfile(target_file_path):
+        extension = os.path.splitext(target_file_path)[1]  # 扩展名
+        new_name = os.path.dirname(target_file_path) + '/rename' + extension  # 重命名为rename
+        try:
+            shutil.move(target_file_path, new_name)
+        except IOError:  # 对于被别的程序占用的文件，直接操作会报错
+            print('[文件被占用] = ' + target_file_path)
+
+
+def saved_delete_file(target_file):
     if os.path.isfile(target_file):
         try:
             send2trash.send2trash(target_file)  # 使用send2trash安全删除（丢到回收站）
@@ -27,7 +39,7 @@ def saved_delete_files(target_file):
             print('[文件被占用] = ' + target_file)
 
 
-def move_files_to_new_dir(target_file):
+def move_file_to_new_dir(target_file):
     if not os.path.exists(MOVED_DIR):
         os.mkdir(MOVED_DIR)
     try:
@@ -38,14 +50,14 @@ def move_files_to_new_dir(target_file):
 
 def record_deleted_files_name(target_file):
     print('[Deleted File] = ' + target_file)
-    writeFile = open(DELETED_FILES_NAME, 'a')  # a是追加模式，否则文件会被覆盖
-    writeFile.write(target_file)
-    writeFile.write('\n')
-    writeFile.close()
+    write_file = open(DELETED_FILES_NAME, 'a')  # a是追加模式，否则文件会被覆盖
+    write_file.write(target_file)
+    write_file.write('\n')
+    write_file.close()
 
 
-def delete_file(target_path, target_file):
-    result = DELETE_PATTERN.match(target_file)
+def delete_file(target_path):
+    result = DELETE_PATTERN.match(os.path.basename(target_path))
     if result is not None:  # 找到待删除文件
         do_operation(target_path)
 
@@ -53,7 +65,7 @@ def delete_file(target_path, target_file):
 def find_delete_file(root_dir):
     for root, dirs, files in os.walk(root_dir):
         for f in files:
-            delete_file(os.path.join(root, f), f)
+            delete_file(os.path.join(root, f))
 
 
 if __name__ == '__main__':
